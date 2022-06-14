4d14f1634f206c8efb1b74374f8fe4d82423463f
Qien Nathan Cai <qien.nathan@gmail.com>
2022-06-13 18:52:09 -0700

    Complete game

---
+++ game.js
@@ -0,0 +1,47 @@
+const moves = ['ROCK', 'PAPER', 'SCISSORS'];
+
+function computerPlay() {
+    return moves[Math.floor(Math.random() * 3)];
+};
+
+function playRound(playerSelection, computerSelection) {
+    const playerIndex = moves.indexOf(playerSelection.toUpperCase());
+    if (playerIndex ===  -1) {
+        return -1;
+    }else if (computerSelection.toUpperCase() === playerSelection.toUpperCase()){
+        return 0;
+    }else if (computerSelection.toUpperCase() === moves.at(playerIndex - 1)) {
+        return 1;
+    }else{
+        return 2;
+    }
+};
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Please enter your choice', 'ROCK, PAPER, SCISSORS');
+        let computerSelection = computerPlay();
+        let round = playRound(playerSelection, computerPlay());
+        if (round === -1) {
+            console.log(`Invalid Selection`);
+        } else if (round === 0) {
+            console.log(`You Tied!`);
+        } else if (round === 1) {
+            console.log(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`);
+            playerWins++;
+        } else {
+            console.log(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`);
+            computerWins++;
+        };
+     };
+    if (playerWins > computerWins) {
+        console.log(`You won the game! The score was ${playerWins}-${computerWins}`);
+    }else if (playerWins === computerWins) {
+        console.log(`You tied! The score was ${playerWins}-${computerWins}`);
+    } else {
+        console.log(`You lost the game! The score was ${playerWins}-${computerWins}`);
+    }
+}
+game()
