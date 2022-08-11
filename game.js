0865389552cd839a7508a401246b0e46a71ec6af
Qien Nathan Cai <qien.nathan@gmail.com>
2022-08-10 20:47:16 -0700

    Change game to browser game

--- game.js
+++ game.js
@@ -1,49 +1,63 @@
 const moves = ['ROCK', 'PAPER', 'SCISSORS'];
 
+let score = [0,0];
+const scissor = document.querySelector('#scissor');
+const paper = document.querySelector('#paper');
+const rock = document.querySelector('#rock');
+
+const body = document.querySelector('body');
+const again = document.createElement('button');
+again.setAttribute('id', 'again');
+again.setAttribute('onclick', 'playAgain()')
+again.innerHTML = 'Play Again!'
+const dScore = document.querySelector("#score");
+const dMessage = document.querySelector("#message");
+
 function computerPlay() {
     return moves[Math.floor(Math.random() * 3)];
 };
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, score) {
     const playerIndex = moves.indexOf(playerSelection.toUpperCase());
-    if (playerIndex ===  -1) {
-        return -1;
-    }else if (computerSelection.toUpperCase() === playerSelection.toUpperCase()){
-        return 0;
-    }else if (computerSelection.toUpperCase() === moves.at(playerIndex - 1)) {
-        return 1;
-    }else{
-        return 2;
+    if (score[0] !== 3 && score[1] !== 3) {
+        if (computerSelection.toUpperCase() === playerSelection.toUpperCase()){
+            return 0;
+        }else if (computerSelection.toUpperCase() === moves.at(playerIndex - 1)) {
+            score[0]++
+            return 1;
+        }else{
+            score[1]++
+            return 2;
+        }
     }
 };
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Please enter your choice', 'ROCK, PAPER, SCISSORS');
-        let computerSelection = computerPlay();
-        let round = playRound(playerSelection, computerSelection);
-        if (round === -1) {
-            console.log(`Invalid Selection`);
-            i--;
-        } else if (round === 0) {
-            console.log(`You Tied!`);
-            i--;
-        } else if (round === 1) {
-            console.log(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`);
-            playerWins++;
-        } else {
-            console.log(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`);
-            computerWins++;
-        };
-     };
-    if (playerWins > computerWins) {
-        console.log(`You won the game! The score was ${playerWins}-${computerWins}`);
-    }else if (playerWins === computerWins) {
-        console.log(`You tied! The score was ${playerWins}-${computerWins}`);
-    } else {
-        console.log(`You lost the game! The score was ${playerWins}-${computerWins}`);
-    }
-}
-game()
+function playAgain() {
+    score = [0,0];
+    dMessage.innerHTML = '';
+    dScore.innerHTML = `${score[0]}-${score[1]}`;
+    body.removeChild(again);
+};
+
+function game(playerSelection, score) {
+    let computerSelection = computerPlay()
+    let round = playRound(playerSelection, computerSelection, score);
+    if (round === 0) {
+        dMessage.innerHTML =`You Tied!`;
+    } else if (round === 1) {
+        dMessage.innerHTML = `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
+    } else if (round === 2) {
+        dMessage.innerHTML =`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
+    };
+
+    dScore.innerHTML = `${score[0]}-${score[1]}`
+
+    if (score[0] === 3) {
+        dMessage.innerHTML = "You won the game!";
+        body.appendChild(again);
+    } else if (score[1] === 3) {
+        dMessage.innerHTML = "You lost the game!";
+        body.appendChild(again);
+    };
+
+};
