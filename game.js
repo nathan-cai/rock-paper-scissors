1b6131a185419ac100effb658c5e448499c7b4c2
Qien Nathan Cai <qien.nathan@gmail.com>
2022-08-25 15:32:59 -0700

    Add code to highlight Computer's choice

--- game.js
+++ game.js
@@ -1,11 +1,18 @@
-const moves = ['ROCK', 'PAPER', 'SCISSORS'];
+const moves = ['Rock', 'Paper', 'Scissor'];
 
 let score = [0,0];
+let computerSelection = '';
+
 const scissor = document.querySelector('#scissor');
 const paper = document.querySelector('#paper');
 const rock = document.querySelector('#rock');
+const cRock = document.querySelector('#cRock');
+const cPaper = document.querySelector('#cPaper');
+const cScissor = document.querySelector('#cScissor');
 
 const body = document.querySelector('body');
+const mid = document.querySelector('#mid')
+
 const again = document.createElement('button');
 again.setAttribute('id', 'again');
 again.setAttribute('onclick', 'playAgain()')
@@ -17,12 +24,18 @@ function computerPlay() {
     return moves[Math.floor(Math.random() * 3)];
 };
 
-function playRound(playerSelection, computerSelection, score) {
-    const playerIndex = moves.indexOf(playerSelection.toUpperCase());
+function playRound(playerSelection, score) {
+    const playerIndex = moves.indexOf(playerSelection);
     if (score[0] !== 3 && score[1] !== 3) {
-        if (computerSelection.toUpperCase() === playerSelection.toUpperCase()){
+        cPaper.classList.remove('chosen');
+        cRock.classList.remove('chosen');
+        cScissor.classList.remove('chosen');
+        computerSelection = computerPlay()
+        document.querySelector(`#c${computerSelection}`).classList.add('chosen');
+
+        if (computerSelection === playerSelection){
             return 0;
-        }else if (computerSelection.toUpperCase() === moves.at(playerIndex - 1)) {
+        }else if (computerSelection === moves.at(playerIndex - 1)) {
             score[0]++
             return 1;
         }else{
@@ -34,30 +47,32 @@ function playRound(playerSelection, computerSelection, score) {
 
 function playAgain() {
     score = [0,0];
+    cPaper.classList.remove('chosen');
+    cRock.classList.remove('chosen');
+    cScissor.classList.remove('chosen');
     dMessage.innerHTML = '';
     dScore.innerHTML = `${score[0]}-${score[1]}`;
-    body.removeChild(again);
+    mid.removeChild(again);
 };
 
 function game(playerSelection, score) {
-    let computerSelection = computerPlay()
-    let round = playRound(playerSelection, computerSelection, score);
+    let round = playRound(playerSelection, score);
     if (round === 0) {
         dMessage.innerHTML =`You Tied!`;
     } else if (round === 1) {
-        dMessage.innerHTML = `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
+        dMessage.innerHTML = `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if (round === 2) {
-        dMessage.innerHTML =`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
+        dMessage.innerHTML =`You Lose! ${computerSelection} beats ${playerSelection}`;
     };
 
     dScore.innerHTML = `${score[0]}-${score[1]}`
 
     if (score[0] === 3) {
         dMessage.innerHTML = "You won the game!";
-        body.appendChild(again);
+        mid.appendChild(again);
     } else if (score[1] === 3) {
         dMessage.innerHTML = "You lost the game!";
-        body.appendChild(again);
+        mid.appendChild(again);
     };
 
 };
